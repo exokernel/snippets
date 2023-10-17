bb3f6d53ee398c3f9b3c359a90ed31e829e0da5c
Dustin Cannon <dustin.cannon@bigcommerce.com>
2023-10-16 20:04:10 -0700

    retile after two splits

--- scripts/bcmux.sh
+++ scripts/bcmux.sh
@@ -20,8 +20,14 @@ while getopts "s" option; do
 done
 
 split_list=()
+i=1
 for ssh_ip in "${ssh_list[@]:1}"; do
     split_list+=("split-pane" "ssh $ssh_ip" ";")
+    i=$(($i+1))
+    # every nth split re-tile the panes
+    if [ $i -ne ${#ssh_list[@]} ] && [ $(( $i % 2 )) -eq 0 ]; then
+        split_list+=("select-layout" "tiled" ";")
+    fi
 done
 
 if [ $sync == true ]; then
